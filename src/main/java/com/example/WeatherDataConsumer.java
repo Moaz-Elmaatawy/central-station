6023ed19564226812e5077a843afdddd27f0d263
moaz nabil <moaznabil886@gmail.com>
2023-05-10 08:21:51 +0300

    Refactor writing to parquet

--- src/main/java/com/example/WeatherDataConsumer.java
+++ src/main/java/com/example/WeatherDataConsumer.java
@@ -1,85 +1,37 @@
 package com.example;
 
-import com.example.Writers.JsonToParquetWriter;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.spark.sql.SparkSession;
-import org.json4s.jackson.Json;
 
-import java.lang.reflect.Type;
-import java.util.Map.Entry;
-
-import java.sql.Timestamp;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Properties;
-import java.util.Set;
 
 public class WeatherDataConsumer {
+    private static final int POLLING_TIME = 1000;
     public static void main(String[] args) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
         props.setProperty("group.id", "weather-data-consumer");
         props.setProperty("key.deserializer", StringDeserializer.class.getName());
         props.setProperty("value.deserializer", StringDeserializer.class.getName());
-        SparkSession spark = SparkSession.builder()
-                .appName("ReadJsonArrayToDataFrame")
-                .master("local[*]")  // Set the master URL for the local mode
-                .getOrCreate();
-        int batchSize=10000;
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Collections.singletonList("weather_data"));
-        int counter=0;
-        HashMap<Integer,StringBuilder>stationsData=new HashMap<>();
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-            for (ConsumerRecord<String, String> record : records) {
-                System.out.println("Received message: " + record.value());
-                int stationID=Integer.parseInt(record.value().split(",")[0].split(":")[1]);
-                System.out.println("Station ID is ==== "+stationID);
-                if(stationsData.containsKey(stationID)){
-                    stationsData.put(stationID, stationsData.get(stationID).append(",\n").append(record.value()));
-                }else {
-                    stationsData.put(stationID,new StringBuilder(record.value()));
-                }
-                counter++;
-                System.out.println("counter === "+counter);
-                if(counter>=batchSize){
-                    writeDataInParquetFiles(stationsData,spark);
-                    stationsData.clear();
-                    counter=0;
-                }
+        
+
+        WeatherStatusArchiver archiver = new WeatherStatusArchiver();
+        
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(Collections.singletonList("weather_data"));
+            
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(POLLING_TIME));
+                archiver.archiveRecord(records);
+
             }
+        
+        }
+        catch(Exception e){
+            System.out.println(e);
         }
     }
-    public static String getCurrentDate(long time){
-        Timestamp ts=new Timestamp(time);
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        return dateFormat.format(ts);
-    }
-    public static String getCurrentTIme(long time){
-        Timestamp ts=new Timestamp(time);
-        DateFormat timeFormat = new SimpleDateFormat("hh-mm-ss-SSS");
-        return timeFormat.format(ts);
-    }
-    public static void writeDataInParquetFiles(HashMap<Integer,StringBuilder>data,SparkSession sparkSession){
-        Set<Entry<Integer, StringBuilder> > entrySet = data.entrySet();
-        for (Entry<Integer, StringBuilder> entry : entrySet) {
-            long ts=Long.parseLong(entry.getValue().toString().split(",")[3].split(":")[1]);
-            String outputPath="stationID_"+entry.getKey()+"/"+getCurrentDate(ts)+"/"+getCurrentTIme(ts)+".parquet";
-            String jsonRecords="[\n"+entry.getValue()+"\n]";
-            System.out.println(jsonRecords);
-            JsonToParquetWriter.writeParquet(jsonRecords,outputPath,sparkSession);
-        }
-        data.clear();
-    }
-
-
 }
---
+++ src/main/java/com/example/WeatherStatusArchiver.java
@@ -0,0 +1,73 @@
+package com.example;
+
+import com.example.Writers.JsonToParquetWriter;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Map.Entry;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Set;
+
+public class WeatherStatusArchiver {
+    private final int BATCH_SIZE=10000;
+    int counter=0;
+    HashMap<Integer,StringBuilder>stationData;
+    SparkSession spark;
+
+    public WeatherStatusArchiver(){
+        stationData=new HashMap<>();
+        spark = SparkSession.builder()
+                .appName("ReadJsonArrayToDataFrame")
+                .master("local[*]")  // Set the master URL for the local mode
+                .getOrCreate();
+    }
+
+    public void archiveRecord(ConsumerRecords<String, String> records){
+
+        for (ConsumerRecord<String, String> record : records) {
+            System.out.println("Received message: " + record.value());
+            int stationID=Integer.parseInt(record.value().split(",")[0].split(":")[1]);
+            System.out.println("Station ID is ==== "+stationID);
+            if(stationData.containsKey(stationID)){
+                stationData.put(stationID, stationData.get(stationID).append(",\n").append(record.value()));
+            }else {
+                stationData.put(stationID,new StringBuilder(record.value()));
+            }
+            counter++;
+            System.out.println("counter === "+counter);
+            if(counter >= BATCH_SIZE){
+                writeDataInParquetFiles(stationData,spark);
+                stationData.clear();
+                counter=0;
+            }
+        }
+    }
+
+    public String getCurrentDate(long time){
+        Timestamp ts=new Timestamp(time);
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(ts);
+    }
+    public String getCurrentTIme(long time){
+        Timestamp ts=new Timestamp(time);
+        DateFormat timeFormat = new SimpleDateFormat("hh-mm-ss-SSS");
+        return timeFormat.format(ts);
+    }
+    public void writeDataInParquetFiles(HashMap<Integer,StringBuilder>data,SparkSession sparkSession){
+        Set<Entry<Integer, StringBuilder> > entrySet = data.entrySet();
+        for (Entry<Integer, StringBuilder> entry : entrySet) {
+            long ts=Long.parseLong(entry.getValue().toString().split(",")[3].split(":")[1]);
+            String outputPath="stationID_"+entry.getKey()+"/"+getCurrentDate(ts)+"/"+getCurrentTIme(ts)+".parquet";
+            String jsonRecords="[\n"+entry.getValue()+"\n]";
+            System.out.println(jsonRecords);
+            JsonToParquetWriter.writeParquet(jsonRecords,outputPath,sparkSession);
+        }
+        data.clear();
+    }
+}
